028e204edaddda58b3e3c19710d893faf645cf10
Katiasun <katjatolok3@gmail.com>
2024-05-02 21:27:10 +0200

    I installed 2 new packages. Created new folders.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,5 @@ import React from "react";
 import WeatherApp from "./components/WeatherApp/WeatherApp";
 
 export default function App() {
-  return (
-    <div className="App">
-      <WeatherApp />
-    </div>
-  );
+  return <div className="App">{/* <WeatherApp /> */}</div>;
 }
