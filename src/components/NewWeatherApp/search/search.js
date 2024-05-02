c413ddbb685d38126c5155c7e1912a96d9264710
Katiasun <katjatolok3@gmail.com>
2024-05-02 22:01:04 +0200

    initializes a state variable named search using the useState hook. declares a function named handleOnChange.

--- src/components/NewWeatherApp/search/search.js
+++ src/components/NewWeatherApp/search/search.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 
-export default function Search() {
+export default function Search({ onSearchChange }) {
+  const [search, setSearch] = useState(null);
+
+  function handleOnChange(searchData) {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  }
+
   return (
     <AsyncPaginate
       placeholder="Search for city"
